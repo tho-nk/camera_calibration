a7a9c2606e7a4b6400861ac84000f0e8fdbcb17b
Tho NGUYEN KHAC <tho.nguyenkhac1325@gmail.com>
2023-11-22 11:55:43 +0100

    third-party

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,9 @@
 export project_name=bilberry
+export THIRD_PARTY=/home/ugitho/third-party
+export Boost_Version='1.81.0'
+export GoogleTest_Version='1.13.0'
+export OpenCV_Version='4.8.0'
+export Rapidjson_Version='1.1.0'
 
 cd ${project_name}
 
